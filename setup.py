9f62356545d884d8b1393b6f0162f6aef0d5dea0
Shawn Niederriter <shawn.niederriter@temple.edu>
2017-05-08 07:26:01 +0000

    Fixed setup.py error

--- setup.py
+++ setup.py
@@ -41,5 +41,5 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages='lottery_system'
+    py_modules='lottery_system'
 )
